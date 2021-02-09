22d5632763a7a270892780d373f41b9d2eceb8f9
OzTamari <oz.tamari@whitesourcesoftware.com>
2021-02-09 10:10:53 +0200

    1st commit includin unitesting!

--- ws_sdk/tests/testWeb.py
+++ ws_sdk/tests/testWeb.py
@@ -1,5 +1,5 @@
 import unittest
-from mock import MagicMock, patch
+from mock import patch
 from web import WSS
 
 c = WSS(api_url="wss_api_url", user_key="user_key1", token="org_token1", token_type="organization")
@@ -14,9 +14,9 @@ class TestWeb(unittest.TestCase):
         res = c.__create_body__("api_call", {"key": "Value"})
         self.assertIsInstance(res, dict)
 
-    @patch('web.requests')
+    @patch('web.requests')                            # Injecting mock for requests module. Object name is mock_request
     def test_call_api(self, mock_requests):
-        with patch('web.requests.post') as patched_post:
+        with patch('web.requests.post') as patched_post:  # Mocking post method so return value will include status_code attribute
             patched_post.return_value.status_code = 200
             patched_post.return_value.text = '{"key": "val"}'
             res = c.__call_api__("api_call")
