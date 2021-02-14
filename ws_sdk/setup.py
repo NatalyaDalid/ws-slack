687ca1af3453865a27a89642ce6a5d321c429380
OzTamari <oz.tamari@whitesourcesoftware.com>
2021-02-14 22:11:08 +0200

    CI

--- ws_sdk/setup.py
+++ ws_sdk/setup.py
@@ -9,10 +9,15 @@ setuptools.setup(
     author="WS PS",
     author_email="oz.tamari@whitesourcesoftware.com",
     description="WS Python SDK",
-    url='http://pypi.python.org/pypi/PackageName/',
+    url='https://github.com/whitesource-ps/ws_sdk',
     license='LICENSE.txt',
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
     long_description=open("README.md").read(),
-    install_requires=[],
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
